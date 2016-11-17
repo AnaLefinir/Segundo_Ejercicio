166df10e2bc6898a26476a0ae2b4491c877267b8
Ana Lefinir <analefinir@gmail.com>
2016-11-17 11:51:43 -0300

    Cambios realizados: ahora evalua los campos de la form y actua acorde a ello, paso siguiente es sumarles unas modals para avisar con un mensaje si es invalido el campo requerido

--- ajax/public/client.js
+++ ajax/public/client.js
@@ -7,7 +7,14 @@ $(document).ready(function () {
 
     $('form').on('submit', addTask);
 
+    $('input').focus(focusInput)
+              .blur(blurInput);
+
+    $('textarea').on('click', onTextarea)
+
+
 });
+
 
 function appendTasks(tasks) {
     getTemplateAjax('/templates/demo.handlebars', function (template) {
@@ -24,7 +31,10 @@ function appendTasks(tasks) {
             }
         }
 
-        $('.del').on('click', function (event) {
+        $('.del').on('click', addModalToDeleteBtn);
+        $('.check-element').on('change', updateTaskStatus);
+
+        function addModalToDeleteBtn(event) {
             var target = $(event.currentTarget);
             var titleTask;
             var id = $(this).data('id');
@@ -37,19 +47,15 @@ function appendTasks(tasks) {
             $(".modal-body #strongTitle").text(titleTask);
 
 
-
             $('#confirm-delete').modal('toggle');
 
             $('.btn-ok').one('click', function () {
                 removeTask(target.data('id'));
                 target.parents('.task').remove();
                 $("#confirm-delete").modal('hide');
-            });
+            })
+        }
 
-        });
-
-
-        $('.check-element').on('change', updateTaskStatus);
     });
 }
 
@@ -72,19 +78,65 @@ function getTemplateAjax(path, callback) {
 
 function addTask(event) {
     event.preventDefault();
-    var form = $(this);
-    var taskData = form.serialize();
 
-    $.ajax({
-        type: 'POST',
-        url: '/api/tasks',
-        data: taskData,
-        success: function (tasks) {
-            appendTasks(tasks);
-            form.trigger('reset');
-        },
-        error: showAjaxError
-    });
+    var $form = $(this);
+    var $inputRequired = $form.find('input.form-control');
+    var $inputRequiredTitle = $inputRequired.val();
+    var $inputRequiredData = $inputRequired.data('pass');
+    var $divContainerInput = $inputRequired.parents('#input-required');
+
+
+    if (($inputRequiredData === 'false' || $inputRequiredTitle === '') || ($inputRequiredData === '' && $inputRequiredTitle === '')) {
+
+        $divContainerInput.data('pass', 'false');
+        //PUT MODAL FOR INFORM
+
+    } else if (($inputRequiredData === '' && $inputRequiredTitle !== '') || $inputRequiredData === 'true' && $inputRequiredTitle !== '') {
+
+        if ($inputRequiredData === 'true') {
+
+            ajaxCall();
+
+        } else if ($inputRequiredData === '') {
+
+            var title = $inputRequired.val();
+            var resultRegex = validateAlphanumeric(title);
+
+            if (resultRegex === false) {
+
+                $divContainerInput.data('pass', 'false');
+                $divContainerInput.addClass('has-error');
+                //PUT MODAL FOR INFORM
+
+            } else {
+
+                ajaxCall();
+            }
+        }
+
+    } else {
+        console.log('esto es un error!');
+    }
+
+
+    function ajaxCall() {
+
+        var taskData = $form.serialize();
+
+        $divContainerInput.removeClass('has-success');
+        $divContainerInput.data('pass', 'false');
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/tasks',
+            data: taskData,
+            success: function (tasks) {
+                appendTasks(tasks);
+                $form.trigger('reset');
+            },
+            error: showAjaxError
+        });
+    }
 }
 
 function removeTask(id) {
@@ -97,10 +149,6 @@ function removeTask(id) {
         },
         error: showAjaxError
     });
-}
-
-function showAjaxError(xhr, status, error) {
-    console.log('[' + status + '] ' + error);
 }
 
 function updateTaskStatus($event) {
@@ -125,3 +173,44 @@ function updateTaskStatus($event) {
         error: showAjaxError
     })
 }
+
+function blurInput() {
+    var $target = $(this);
+    var textInput = $target.val();
+    var $targetParentDiv = $target.parent('div');
+    var resultRegex = validateAlphanumeric(textInput);
+
+    if (!resultRegex) {
+        $targetParentDiv.addClass('has-error');
+        $target.data('pass', 'false');
+    } else {
+        $targetParentDiv.addClass('has-success').removeClass('has-error');
+        $target.data('pass', 'true');
+    }
+}
+
+function focusInput() {
+    var $target = $(this);
+    $target.parent('div').addClass('has-error');
+}
+
+function onTextarea() {
+    var $target = $(this);
+    var $requiredInput = $target.closest('form').find('input.form-control');
+
+    if ($requiredInput.data('pass') === 'false' || $requiredInput.data('pass') === '') {
+        $requiredInput.parents('#input-required').addClass('has-error');
+    }
+}
+
+function showAjaxError(xhr, status, error) {
+    console.log('[' + status + '] ' + error);
+}
+
+function validateAlphanumeric(string) {
+    var regex = /^[-a-z0-9,\/()&:. ]*[a-z][-a-z0-9,\/()&:. \!]*$/;
+    var reSource = regex.source;
+    var regexFinal = new RegExp(reSource, 'i');
+
+    return regexFinal.test(string);
+};
